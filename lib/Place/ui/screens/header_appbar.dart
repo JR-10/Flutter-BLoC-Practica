d7e5207872e94095323e398417785272d70c8258
Jonathan Rivera <arbeyjr@gmail.com>
2021-02-25 13:50:21 -0500

    Implementar BlocProvider

--- lib/Place/ui/screens/header_appbar.dart
+++ lib/Place/ui/screens/header_appbar.dart
@@ -5,7 +5,6 @@ import '../widgets/card_image_list.dart';
 class HeaderAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Stack(
       children: <Widget>[GradientBack("Bienvenido"), CardImageList()],
     );
